e133a77bc07f6376a6063d27e59c296766d7fa34
杨坤海 <384037191@qq.com>
2019-11-26 13:37:13 +0800

    rm .tea

--- .tea/entryFiles-development/index$.web.js
+++
@@ -1,5 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-
-require('../../components/range/range?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
-require('../../pages/index/index?hash=faa6e0657b2663cc9d3aa48d6e8cf55df5f423c0');
